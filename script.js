c48a879ead9d25b89e2be5479ab7e33f4e34c495
thfilipe <thomasfilipe@yahoo.com>
2022-06-26 10:55:02 +0100

    Add win conditions

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function computerPlay() {
-    const rockPaperScissors = ["Rock", "Paper", "Scissors"]
+    const rockPaperScissors = ["rock", "paper", "scissors"]
     const random = Math.floor(Math.random() * rockPaperScissors.length)
     return rockPaperScissors[random]
 
@@ -8,7 +8,21 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = prompt("Please type Rock, Paper or Scissors:").toLowerCase();
     computerSelection = computerPlay();
-
-    return playerSelection
+    console.log("Player: " + playerSelection + " " + "Computer: " + computerSelection)
+    if (playerSelection === computerSelection) {
+        return "Tie!"
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You Win! Rock beats Scissors"
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You Win! Paper beats Rock"
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You Win! Scissors beats Paper"
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You Lose! Paper beats Rock"
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You Lose! Rock beats Scissors"
+    } else {
+        return "You Lose! Scissors beats Paper"
+    }
 }
 console.log(playRound());
