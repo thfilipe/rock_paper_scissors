5cc3d6f1b2a291b7617ab1f8319388dbdaaa0a54
thfilipe <thomasfilipe@yahoo.com>
2022-07-03 08:26:27 +0100

    Comment out game rounds

--- script.js
+++ script.js
@@ -31,19 +31,19 @@ function game() {
     let playerScore = 0;
     let computerScore = 0;
 
-    for (let i = 0; i < 5; i++) { // Play through 5 rounds
+    // for (let i = 0; i < 5; i++) { // Play through 5 rounds
 
-        let outcome = playRound();
-        console.log(outcome)
-        if (outcome === "Tie!") {
+    //     let outcome = playRound();
+    //     console.log(outcome)
+    //     if (outcome === "Tie!") {
 
-        } else if (outcome == "You Win! Rock beats Scissors" || outcome == "You Win! Paper beats Rock" || outcome == "You Win! Scissors beats Paper") {
-            playerScore++;
-        } else {
-            computerScore++;
-        }
+    //     } else if (outcome == "You Win! Rock beats Scissors" || outcome == "You Win! Paper beats Rock" || outcome == "You Win! Scissors beats Paper") {
+    //         playerScore++;
+    //     } else {
+    //         computerScore++;
+    //     }
 
-    }
+    // }
     if (playerScore === computerScore) {
         console.log("Player Score: " + playerScore + "\nComputer Score: " + computerScore + "\nGame is a Tie!")
     } else if (playerScore > computerScore) {
