948b087eb5a9f6efa9e8e1c3fb6ed704ff088bb1
Luke Howkins <luke.howkins@yoti.com>
2022-07-04 14:17:37 +0100

    extract winningRoundsCuttoff into own variable, make playRound args consistent

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ let computerScore = 0;
 
 // player choice buttons
 const rockBtn = document.getElementById("rock");
-rockBtn.addEventListener("click", () => playRound("rock", computerPlay));
+rockBtn.addEventListener("click", () => playRound("rock", computerPlay()));
 const paperBtn = document.getElementById("paper");
-paperBtn.addEventListener("click", () => playRound("paper", computerPlay));
+paperBtn.addEventListener("click", () => playRound("paper", computerPlay()));
 const scissorsBtn = document.getElementById("scissors");
-scissorsBtn.addEventListener("click", () => playRound("scissors", computerPlay));
+scissorsBtn.addEventListener("click", () => playRound("scissors", computerPlay()));
 
 const caption = document.getElementById("caption")
+const rockPaperScissors = ["rock", "paper", "scissors"]
+const winningRoundsCuttoff = 5
 
 // computer random choice
 function computerPlay() {
-    const rockPaperScissors = ["rock", "paper", "scissors"]
     const random = Math.floor(Math.random() * rockPaperScissors.length)
     return rockPaperScissors[random]
-
 }
 
 // player and computer scoring
@@ -26,7 +26,7 @@ function scoring() {
     const divComputer = document.getElementById("computer-score");
     if (caption.textContent == "Tie!") {
         return;
-    } else if (caption.textContent === 'You Win! Rock beats Scissors' || caption.textContent === "You Win! Paper beats Rock" || caption.textContent === "You Win! Scissors beats Paper") {
+    } else if (caption.textContent.includes('You Win!')) {
         playerScore++;
         divPlayer.textContent = `Player Score: ${playerScore}`
     } else {
@@ -35,19 +35,17 @@ function scoring() {
     }
 }
 
-// winner when 5 point is reached
+// winner when max points is reached
 function winner() {
-    if (playerScore == 5) {
+    if (playerScore == winningRoundsCuttoff) {
         caption.textContent = "Player Wins!";
-    } else if (computerScore == 5) {
+    } else if (computerScore == winningRoundsCuttoff) {
         caption.textContent = "Computer Wins!";
     }
 }
 
 // play a round of rock paper scissors
 function playRound(playerSelection, computerSelection) {
-    computerSelection = computerPlay();
-
     if (playerSelection === computerSelection) {
         caption.textContent = "Tie!";
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
@@ -64,10 +62,5 @@ function playRound(playerSelection, computerSelection) {
         caption.textContent = "You Lose! Scissors beats Paper";
     }
     scoring();
-    if (playerScore == 5 || computerScore == 5) {
-        winner();
-    }
-
+    winner();
 }
-
-
